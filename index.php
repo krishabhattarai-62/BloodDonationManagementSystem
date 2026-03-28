b75d0013f3d70f730c17f787aac214da4071e58d
rx1002 <prajwaldulal258@gmail.com>
2026-03-28 23:34:55 +0545

    Created header and footer

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="index-wrapper">
+
+    <div class="blood-icon">🩸</div>
+
+    <h1>Blood Donation Management System</h1>
+
+    <p>Save lives by donating blood. Please login or register.</p>
+
+    <div class="index-btn-group">
+        <a href="public/login.php"><button>Login</button></a>
+        <a href="public/register.php"><button>Register</button></a>
+    </div>
+
+</div>
+
+<?php include 'includes/footer.php'; ?>
